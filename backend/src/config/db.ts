124ca7cb7b66c250de2b4e807a232bd6f5dde3a2
Miguel Villa Ríos <mvilla2019@inf.udec.cl>
2026-02-04 13:48:38 -0300

    Sección 23: Init

---
+++ backend/src/config/db.ts
@@ -0,0 +1,10 @@
+import { Sequelize } from "sequelize-typescript";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const db = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: { require: false },
+  },
+});
---
+++ backend/src/server.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import colors from "colors";
+import morgan from "morgan";
+import { db } from "./config/db";
+
+async function connectDB() {
+  try {
+    await db.authenticate();
+    db.sync();
+    console.log(colors.blue.bold("Conexión exitosa a la BD"));
+  } catch (error) {
+    //console.log(error);
+    console.log(colors.red.bold("Error de conexión a la BD"));
+  }
+}
+
+connectDB();
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+export default app;
